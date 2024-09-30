cad8c4d9f7b212c1b498a799bf8b35c540c8bdd0
Rich <horich21@gmail.com>
2024-09-30 03:32:54 -0700

    Initial Commit

---
+++ server/src/main/java/com/example/Spring/Boot/AI/Integration/SpringBootAiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Boot.AI.Integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAiIntegrationApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/Spring/Boot/AI/Integration/SpringBootAiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Boot.AI.Integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
